b211715643c3afbb94e9070be0810bab57f244c8
gynt <Gynt@users.noreply.github.com>
2025-02-19 23:02:39 +0100

    feat(meta): add ucp3 extension requirements

---
+++ init.lua
@@ -0,0 +1,13 @@
+
+local dll
+
+return {
+  enable = function(self, config)
+    dll = require("cffi.dll")
+  end,
+  
+  cffi = function(self) return dll end,
+  
+  disable = function(self, config) end,
+
+}
---
+++ locale/description.md
@@ -0,0 +1,2 @@
+# CFFI
+This extension is a module providing a C interface to game memory. Main use case is as a dependency for other modules.
