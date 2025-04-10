b69a93e7a21bafe3b9667453dacf723bec7a1899
Gynt <Gynt@users.noreply.github.com>
2025-04-11 01:24:33 +0200

    feat(cffi): add documentation and wrap dll

--- init.lua
+++ init.lua
@@ -1,16 +1,31 @@
+local cffi = {}
 
 local dll
 
-return {
-  enable = function(self, config)
-    dll = require("cffi.dll")
-  end,
-  
-  cffi = function(self) return dll end,
-  
-  disable = function(self, config) end,
+---@class CFFIInterface
+local CFFIInterface = {}
 
-}, {
+---Declare c types (globally)
+---@param contents string The c-syntax header declarations
+---@return nil nothing
+function CFFIInterface.cdef(contents)
+  if dll == nil then error('cffi.dll not initialized') end
+  return dll.cdef(contents)
+end
+
+---Get a handle to the cffi lua library
+---@return CFFIInterface cffi
+function cffi:cffi() return CFFIInterface end
+
+
+function cffi.enable(self, config)
+  dll = require("cffi.dll")
+end
+  
+  
+function cffi.disable(self, config) end
+
+return cffi, {
   proxy = {
     ignored = "cffi",
   },
