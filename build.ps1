88d5fa673b940e9a85c1d1c8e772fe0ab7ba74b3
Gynt <gynt@users.noreply.github.com>
2025-08-22 16:27:23 +0200

    CI: include check for presence of meson

--- build.ps1
+++ build.ps1
@@ -23,6 +23,11 @@ cp ".\lua\build\native\bin\Win32\v143\$BuildType\lua.dll" deps\lua.dll
 cp ".\lua\build\native\lib\Win32\v143\$BuildType\lua.lib" deps\lua.lib
 cp -r .\lua\build\native\include\* deps\include
 
+python -m pip show meson
+if ($LASTEXITCODE -ne 0) {
+    python -m pip install meson
+}
+
 meson setup .. -Dlua_version=vendor -Dtests=false --buildtype=$BuildTypeLC --vsenv
 meson compile -C . --vs-args /Zc:threadSafeInit-
 #ninja all
